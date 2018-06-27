ce56199cdfe97ae0514a84e4074d341f82876dd2
Sebastian <sebastian.mofre@gmail.com>
2018-06-27 13:36:23 -0400

    Testing Images
    Using the Expo App

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <Image
+          source={require('./salty.gif')}
+        />
+        <Text>Easy Table!</Text>
+        <Text>JOJO this APP</Text>
+        
       </View>
     );
   }
